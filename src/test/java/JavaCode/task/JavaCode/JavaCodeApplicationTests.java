e5831b27a648eeafa96da951a5278ae9410ca926
DiplineRU <artgul52@gmail.com>
2024-12-06 16:15:47 +0300

    initial commit

---
+++ src/test/java/JavaCode/task/JavaCode/JavaCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package JavaCode.task.JavaCode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaCodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
